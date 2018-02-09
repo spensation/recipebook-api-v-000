81593327e2d18bdfb99daf0b6e1c2191f2be816c
Spencer Evans <github email address>
2018-02-09 02:26:51 -0800

    added create method to RecipeController

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,9 +4,19 @@ class Api::V1::RecipesController < ApplicationController
     render json: @recipes
   end
 
+  def create
+    @recipe = Recipe.create(recipe_params)
+    render json: @recipe
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
     render json: @recipe.to_json(:only => [:title, :category, :serves, :id],
                                   :include => [:user => { :only => [:username]}])
   end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :category, :serves, :prep_time, :cook_time, :total_time, :ingredients, :directions)
+    end
 end
